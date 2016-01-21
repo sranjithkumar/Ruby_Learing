4a7b46f181f77c605e92448b1c35d139bdfba422
sranjithkumar <ranjith@juhomi.com>
2016-01-21 15:49:10 +0530

    ex14

---
+++ ex14.rb
@@ -0,0 +1,10 @@
+user_name = ARGV.first
+prompt = '> '
+
+puts "Hi #{user_name}."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user_name}? "
+puts prompt
+likes = $stdin.gets.chomp
+
+puts "likes: #{likes}"
