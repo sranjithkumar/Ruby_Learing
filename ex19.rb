e72ea175b960b302757394e24f09ecab42329842
sranjithkumar <ranjith@juhomi.com>
2016-01-22 16:00:52 +0530

    ex19: functions and variables

---
+++ ex19.rb
@@ -0,0 +1,8 @@
+def cheese_and_crackers(count_of_cheese, box_count)
+  puts "chesse_count: #{count_of_cheese}"
+  puts "box_count : #{box_count}"
+end
+
+cheese_and_crackers(10,20)
+
+cheese_and_crackers(10+10,20+10)
