a2015d4d5162819d0a3c8bd28e212f06e8978f5d
sranjithkumar <ranjith@juhomi.com>
2016-01-25 16:09:55 +0530

    Ex24;

---
+++ ex24.rb
@@ -0,0 +1,24 @@
+poem = <<END
+\tThe lovely world
+  with logic so firmly planted
+cannot discern \n the needs of love
+ nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+END
+
+puts poem
+
+def secret_formula(started)
+  jelly_beans = started * 500
+  jars = jelly_beans / 1000
+  crates = jars / 100
+  return jelly_beans, jars, crates
+end
+
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+puts "With a starting point of: #{start_point}"
+puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates."
