55e971f22f9680a856b309f0e8f6318562537aec
sranjithkumar <ranjith@juhomi.com>
2016-01-28 15:46:25 +0530

    ex29:if

---
+++ ex29.rb
@@ -0,0 +1,22 @@
+cats = 10
+dogs = 15
+loins = 20
+
+
+if cats > dogs
+  puts "cats count is greater than dogs"
+end
+
+if dogs > loins
+  puts "dogs count is greater than loins"
+end
+
+if loins > dogs
+  puts "loins count is greater than dogs"
+end
+
+cats += 5
+
+if cats >= dogs
+  puts "dog and cat are equal"
+end
