eba5104d6b16383674ef4265a691ab42a4b8f52f
sranjithkumar <ranjith@juhomi.com>
2016-02-02 16:02:06 +0530

    Ex40:class and self

--- ex40.rb
+++ ex40.rb
@@ -17,6 +17,23 @@ class Maths
 
 end
 
-math = Maths.new()
+class Foo
+  @@foos = 0
+
+  def initialize
+    @@foos += 1
+  end
+
+  def self.number_of_foos
+   puts  @@foos
+  end
+end
+
+Foo.new
+Foo.new
+Foo.new
+Foo.number_of_foos
+
+math = Maths.new
 math.add()
 math.multiple()
